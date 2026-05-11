85ab2a955a391025be131fa05a820c568c73299d
Darkdays <110347990+Darkdaysdev@users.noreply.github.com>
2026-05-11 21:11:29 +0300

    Create settings.ts

---
+++ src/config/settings.ts
@@ -0,0 +1,12 @@
+export const settings = {
+    Welcome: {
+        token: ['BOT TOKEN'],
+        voice: ['SES KANALI ID'],
+        guildID: 'SUNUCU ID',
+        staff: ['YETKİLİ ROL ID'],
+        unregister: ['KAYITSIZ ROL ID'],
+        botActivity: ['https://dcflow.space'],
+        botActivityType: ['Playing', 'Watching', 'Listening'],
+        botStatus: ['dnd', 'idle'],
+    },
+};
